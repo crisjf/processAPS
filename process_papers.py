a7d37d0078a633f920c9a83d72c2cecb225d1cd0
Cristian Candia <crcandia@dhcp-18-111-4-184.dyn.mit.edu>
2017-10-27 15:54:13 -0400

    fixed numPages

--- process_papers.py
+++ process_papers.py
@@ -15,8 +15,12 @@ for journal in journals:
             pdata = json.loads(pdata)
             title = defaultdict(lambda:'NULL',pdata['title'])
             date = pdata['date'].split('-')
+            if 'numPages' in pdata.keys():
+                numPages = pdata['numPages']
+            else:
+                numPages = 'NULL'
             if len(date)<2:
                 date = ['NULL','NULL']
-            papers_out.append((pdata['identifiers']['doi'],title['value'],title['format'],journal,volume,pdata['issue']['number'],pdata['numPages'],pdata['date'],date[0],date[1]))
+            papers_out.append((pdata['identifiers']['doi'],title['value'],title['format'],journal,volume,pdata['issue']['number'],numPages,pdata['date'],date[0],date[1]))
 papers_out = pd.DataFrame(papers_out,columns=['DOI','title','title_format','JID','volume','issue','numpages','date','year','month'])
 papers_out.to_csv('processed_data/papers_out.csv',index=False,encoding='utf-8')
